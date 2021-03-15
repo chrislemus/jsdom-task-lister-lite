b62b08904c7640ccd3bfb633f1c8d842aaa752b1
chrislemus <icrislemus@gmail.com>
2021-03-15 14:27:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  const todoWrapper = document.getElementById('tasks')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = document.getElementById('new-task-description').value
+    const todo = document.createElement('li')
+    todo.innerHTML = text
+    todoWrapper.appendChild(todo)
+  })
+
 });
